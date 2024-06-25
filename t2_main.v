831ff1490d6f7e59d4df4106edff610e0e9caf17
NathanCidal <143615223+NathanCidal@users.noreply.github.com>
2024-06-24 23:04:55 -0300

    Update t2_main.v

--- t2_main.v
+++ t2_main.v
@@ -9,6 +9,10 @@
 `define TRIGGER 2           //Acionando
 `define ON 3                //Fazendo 'barulho'
 
+`define DOOR_CLOSED       2'b00
+`define DOOR_OPEN         2'b01
+`define DOOR_CLOSED_AGAIN 2'b10
+
 //---------------------------------
 //--                             --
 //--         M O D U L E         --
@@ -45,6 +49,7 @@ module top(
 
     //Wires
     wire [3:0] value;
+    wire [3:0] value_display;
 
 //---------------------------------
 //--                             --
@@ -104,7 +109,7 @@ debouncer DEB_TIME_VALUE_0(
 
 //---------------------------------
 //--                             --
-//--         D U T ' s           --
+//--         D R I V E R S       --
 //--                             --
 //---------------------------------
 
@@ -120,12 +125,149 @@ siren_generator SIREN_GENERATOR_DRIVER(
     enable_siren, half_hz_enable, siren
 );
 
+timer TIMER_DRIVER(
+    clock, reset, start_timer, value, expired, one_hz_enable, half_hz_enable, value_display
+);
+
 //-----------------------------------------------------------------------
 
 //---------------------------------
 //--                             --
-//--       F S M - T O P         --
+//--            F S M            --
 //--                             --
 //---------------------------------
+
+//Inicialização Padrão da FSM
+always @(posedge clock, posedge reset) begin
+    if(reset) begin     
+        EA <= `SET;
+        D_EA <= 2'b0;       //Disarm_EA
+
+        EA_DD <= 2'b0;
+        EA_DP <= 2'b0;
+    end else begin
+        EA <= PE;
+        D_EA <= D_PE;       //Disarm_PE
+
+        EA_DD <= PE_DD;
+        EA_DP <= PE_DP;
+    end 
+end 
+
+//---------------------------------------------------------------------------------------------------
+//--                            
+//--            D E T E C Ç Ã O     -   A B R E    E    F E C H A       
+//--                             
+//---------------------------------------------------------------------------------------------------
+
+reg[1:0] EA_DD;     //Estado Atual Motorista
+reg[1:0] PE_DD;     //Proximo Estado Motorista
+
+reg[1:0] EA_DP;     //Estado Atual   Passageiro
+reg[1:0] PE_DP;     //Proximo Estado Passageiro
+
+always@* begin
+    case(EA_DD)
+        `DOOR_CLOSED:  if(door_driver == 1'b1) PE_DD = `DOOR_OPEN; else PE_DD = `DOOR_CLOSED;
+        `DOOR_OPEN:    if(door_driver == 1'b0) PE_DD = `DOOR_CLOSED; else PE_DD = `DOOR_CLOSED;
+        `DOOR_CLOSED_AGAIN: PE_DD = `DOOR_CLOSED;
+        default: PE_DD = `DOOR_CLOSED;
+    endcase
+
+    case(EA_DP)
+        `DOOR_CLOSED:  if(door_pass == 1'b1) PE_DP = `DOOR_OPEN; else PE_DP = `DOOR_CLOSED;
+        `DOOR_OPEN:    if(door_pass == 1'b0) PE_DP = `DOOR_CLOSED; else PE_DP = `DOOR_CLOSED;
+        `DOOR_CLOSED_AGAIN: PE_DP = `DOOR_CLOSED;
+        default: PE_DP = `DOOR_CLOSED;
+    endcase
+end
+                                                        
+//---------------------------------------------------------------------------------------------------
+
+//Alteração da Nossa FSM Principal, a TOP
+    
+always @* begin
+    if(c_reprogram) begin 
+        PE <= `SET;             //Se o C_Reprogram Ativar, significa que vai direto para Armado
+    end
+    else begin
+        case(EA)
+            `SET:           //Armado
+                if(c_ignition) PE <= `OFF;
+                else if(c_door_driver == 1 ||c_door_pass == 1) PE <= `TRIGGER;
+                else PE <= `SET;
+                    
+            `OFF:           //Desarmado     
+                if(signalDisarm_To_Arm == 1) PE <= `SET;
+                else PE <= `OFF;
+
+            `TRIGGER:       //Acionando (Contando para Acionar) (Acionado)
+                if(c_ignition) PE <= `OFF;
+                else if(expired) PE <= `ON;
+                else PE <= `TRIGGER;
+
+            `ON:            //Tá ligado, BRUHHHHHHHHH           (Ativar Alarme)
+
+            default:
+        endcase
+    end
+end
+
+//---------------------------------------------------------------------------------------------------
+                
+//FSM para Poder atualizar o OFF
+reg [2:0] D_EA;
+reg [2:0] D_PE;
+
+reg signalDisarm_To_Arm;
+
+always @(posedge clock, posedge reset) begin
+    if(reset) begin
+        signalDisarm_To_Arm <= 0;
+    end else begin
+        if(D_EA == 3'd4) begin
+            signalDisarm_To_Arm <= 1;
+        end else begin
+            signalDisarm_To_Arm <= 0;
+        end
+    end
+end
+
+always @* begin
+    if(EA != `OFF && c_reprogram == 0) begin
+        D_PE <= 3'b0;
+    end 
+    else begin  
+        case(D_EA)
+            3'd0:  D_PE <= 3'd1;
+
+            3'd1:  begin
+                if(ignition == 1'd1) D_PE <= 3'd1;
+                else D_PE <= 3'd2;
+            end
+            3'd2:
+                if(ignition == 1'd1) D_PE <= 3'd1; else
+                begin
+                    if(EA_DD == `DOOR_CLOSED_AGAIN) D_PE <= 3'd3;
+                    else D_PE <= 3'd2;
+                end
+
+            3'd3:
+                if(ignition == 1'd1) D_PE <= 3'd1; else
+                begin
+                   if(EA_DD == `DOOR_OPEN) D_PE <= 3'd2;
+                   else begin
+                     if(expired == 1'b1) D_PE <= 3'd4;
+                     else D_PE <= 3'd3;
+                   end
+                end
+
+            3'd4:
+                D_PE <= 3'd0;
+
+            default: D_PE <= 3'b0;
+        endcase
+    end
+end
 
 endmodule
